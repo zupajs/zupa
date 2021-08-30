0dd339a1fa291d0bd0363cbaae09107f21b85398
Zsolt Lengyel <zsolt.lengyel@adnovum.hu>
2021-08-30 09:48:38 +0200

    Use jsonpath-plus in `config get` command

--- src/core-plugins/config-scripts.plugin.js
+++ src/core-plugins/config-scripts.plugin.js
@@ -1,27 +1,25 @@
 prepare(({ projectDep }) => {
-
 	projectDep('jsonpath-plus@6.0.1')
 })
 
-define(({ config, log, script }) => {
+define(({ config, script }) => {
 
 	script('config', async (argv) => {
 
 		return await script.route(argv, {
 			get(argv) {
-
-
 				const configName = argv._
 				if (configName.length !== 1) {
 					throw new Error('Please define one and only one config param to get')
 				}
 
-				const {JSONPath} = require('jsonpath-plus');
+				const { JSONPath } = require('jsonpath-plus');
+
 				return JSONPath({
 					path: configName[0],
-					json: config.get()
+					json: config.get(),
+					wrap: false,
 				})
-
 			},
 
 			async default(argv) {
