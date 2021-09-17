947a10dff21c3ec9d4c3f2520727b4e8c2c48d92
Zsolt Lengyel <zsolt.lengyel@adnovum.hu>
2021-09-17 09:04:01 +0200

    - liquidate packages structure as I want to keep zupa small

--- packages/cli/main.ts
+++
@@ -1,4 +0,0 @@
-import { main } from './src/index'
-import { resolve } from 'path';
-
-main(resolve(process.cwd(), 'project.js'))
--- packages/cli/src/output/types.ts
+++
@@ -1,7 +0,0 @@
-export const LOG_EVENT = 'logged';
-
-export interface LogRecord {
-	level: string;
-	message: string;
-	data: any;
-}
